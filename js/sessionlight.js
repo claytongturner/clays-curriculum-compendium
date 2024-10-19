16ad93d509440cbbe5cd85970787ceb29f57260c
claytongturner <claytongturner@gmail.com>
2024-10-19 02:48:32 -0500

    About me Clean up
    Added pictures w/ tilt and organized folders

--- js/sessionlight.js
+++ js/sessionlight.js
@@ -9,21 +9,21 @@ function closeLightbox(lightboxId) {
 }
 
 // Function to handle 3D tilt effect
-const thumbnails = document.querySelectorAll('.thumbnail');
+const tiltElements = document.querySelectorAll('.thumbnail, .profile-pic'); // Updated to ensure both .thumbnail and .profile-pic are targeted
 
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('mousemove', (e) => {
-        const rect = thumbnail.getBoundingClientRect();
+tiltElements.forEach(element => {
+    element.addEventListener('mousemove', (e) => {
+        const rect = element.getBoundingClientRect();
         const offsetX = e.clientX - rect.left;
         const offsetY = e.clientY - rect.top;
         const centerX = rect.width / 2;
         const centerY = rect.height / 2;
         const rotateX = ((centerY - offsetY) / centerY) * 10;
         const rotateY = ((offsetX - centerX) / centerX) * 10;
-        thumbnail.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+        element.style.transform = `perspective(500px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
     });
 
-    thumbnail.addEventListener('mouseleave', () => {
-        thumbnail.style.transform = 'perspective(500px) rotateX(0deg) rotateY(0deg)';
+    element.addEventListener('mouseleave', () => {
+        element.style.transform = 'perspective(500px) rotateX(0deg) rotateY(0deg)';
     });
 });
